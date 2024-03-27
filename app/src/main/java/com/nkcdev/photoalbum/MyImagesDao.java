128e54a75ec60f99d9741653dc06e663af625668
Nikola Cywinska <nicywi@gmail.com>
2024-03-27 13:35:24 +0100

    added Dao

---
+++ app/src/main/java/com/nkcdev/photoalbum/MyImagesDao.java
@@ -0,0 +1,26 @@
+package com.nkcdev.photoalbum;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface MyImagesDao {
+
+    @Insert
+    void insert(MyImages myImages);
+
+    @Update
+    void update(MyImages myImages);
+
+    @Delete
+    void delete(MyImages myImages);
+
+    @Query("SELECT * FROM my_images ORDER BY image_id ASC")
+    LiveData<List<MyImages>> getAllImages();
+}
