52591e36e0e76d7286bf0b6c376066da81a2d634
Nikola Cywinska <nicywi@gmail.com>
2024-07-18 10:45:50 +0200

    added request Permission

--- app/src/main/java/com/nkcdev/photoalbum/AddImageActivity.java
+++ app/src/main/java/com/nkcdev/photoalbum/AddImageActivity.java
@@ -1,20 +1,32 @@
 package com.nkcdev.photoalbum;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class AddImageActivity extends AppCompatActivity {
 
     private ImageView imageViewAddImage;
-    private EditText editTextAddTitle;
-    private EditText editTextAddDescription;
+    private EditText editTextAddTitle, editTextAddDescription;
     private Button save;
+
+    ActivityResultLauncher<Intent> activityResultLauncherForSelectImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +40,29 @@ public class AddImageActivity extends AppCompatActivity {
         editTextAddDescription  = findViewById(R.id.editTextAddDescription);
         save = findViewById(R.id.buttonSave);
 
+        //register
+        registerActivityForSelectImage();
+
         imageViewAddImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String permission;
+
+                if(Build.VERSION.SDK_INT >= 33){
+                    permission = Manifest.permission.READ_MEDIA_IMAGES;
+                }else {
+                    permission = Manifest.permission.READ_EXTERNAL_STORAGE;
+                }
+
+                if (ContextCompat.checkSelfPermission(AddImageActivity.this,permission) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(AddImageActivity.this, new String [] {permission}, 1);
+                }else {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    //before API 30 -> startActivityForResult
+                    //ActivityResultLauncher
+                    activityResultLauncherForSelectImage.launch(intent);
+                }
 
             }
         });
@@ -41,5 +73,31 @@ public class AddImageActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            //before API 30 -> startActivityForResult
+            //ActivityResultLauncher
+            activityResultLauncherForSelectImage.launch(intent);
+        }
+    }
+
+    public void registerActivityForSelectImage(){
+
+        activityResultLauncherForSelectImage = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        //check if user selected an image
+                    }
+                }
+        );
+
     }
 }
